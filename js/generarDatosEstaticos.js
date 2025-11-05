939a6304a5482eef2ceb49338bd39258588adda5
Jorge Canales <vegiti7777@gmail.com>
2025-11-05 18:22:03 +0100

    pasamos todos los tests menos 3

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -18,7 +18,7 @@ Js2.mostrarDatoenId(Js1.calcularTotalGastos(), "gastos-totales");
 Js2.mostrarDatoenId(Js1.calcularBalance(), "balance-total");
 Js2.mostrarGastoWeb(Js1.listarGastos(), "listado-gastos-completo");
 Js2.mostrarGastoWeb(Js1.filtrarGastos({fechaDesde : "2021-09-01", fechaHasta : "2021-09-30"}), "listado-gastos-filtrado-1");
-//Js2.mostrarGastoWeb(Js1.filtrarGastos({valorMinimo : "50"}), "listado-gastos-filtrado-2");
-//Js2.mostrarGastoWeb(Js1.filtrarGastos({valorMinimo : "200", etiquetasTiene : ["seguros"]}), "listado-gastos-filtrado-3");/
-//Js2.mostrarGastoWeb(Js1.filtrarGastos({valorMaximo : "50", etiquetasTiene : ["comida", "transporte"]}), "listado-gastos-filtrado-4");
+Js2.mostrarGastoWeb(Js1.filtrarGastos({valorMinimo : "50"}), "listado-gastos-filtrado-2");
+Js2.mostrarGastoWeb(Js1.filtrarGastos({valorMinimo : "200", etiquetasTiene : ["seguros"]}), "listado-gastos-filtrado-3");
+Js2.mostrarGastoWeb(Js1.filtrarGastos({valorMaximo : "50", etiquetasTiene : ["comida", "transporte"]}), "listado-gastos-filtrado-4");
 
